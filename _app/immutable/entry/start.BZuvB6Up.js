d8ee3e2b1cefba49dd3d6fc4e4abc99e80cee243
schulmacher <erikschults@gmail.com>
2025-03-01 19:28:47 +0200

    feat: nice border

---
+++ _app/immutable/entry/start.BZuvB6Up.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/Bn2tl-Cx.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/4.DFWdI1JA.js
@@ -0,0 +1,25 @@
+import{t as l,a as m}from"../chunks/DlXdRFVG.js";import{i as u}from"../chunks/CHnW-4pY.js";import{p as c,c as n,a as d,i as o,m as g,j as f,r as v,k as b}from"../chunks/BmyYr5yZ.js";import{b as h}from"../chunks/DUsfHbrN.js";import{u as x}from"../chunks/6WTMOMVF.js";var A=l(`<main id="home-page" class="page-content svelte-10btdgg"><h1></h1> <p>This is the homepage.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptas et enim suscipit dolore
+		facilis eius nihil perspiciatis, aperiam ex fugiat mollitia laboriosam, asperiores voluptatem
+		eos. At tempora dolor libero.</p></main>`);function k(t,s){c(s,!1);let e=g(null);x(()=>o(e),"home");let a="Hello, SvelteKit Static!";u();var i=A(),p=n(i);p.textContent=a,f(26),v(i),h(i,r=>b(e,r),()=>o(e)),m(t,i),d()}export{k as component};
