3ece79b36514645af097319c401545d0b4a063ff
schulmacher <erikschults@gmail.com>
2025-03-08 10:48:39 +0200

    fix: gallery initial width

--- _app/immutable/entry/start.DGlumkcc.js
+++
@@ -1,1 +0,0 @@
-import{l as o,c as r}from"../chunks/Bl4-gs0J.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.Df4Tbt1l.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/BKH47bvz.js";export{o as load_css,r as start};
